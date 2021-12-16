0e8dce27300696be8da274345b8b29bd4eeacf50
garbaczmaciej <furn20102@gmail.com>
2021-12-17 00:22:35 +0100

    Day12 part1 done

---
+++ Day12/solution.py
@@ -0,0 +1,57 @@
+
+def is_small(cave_name: str) -> bool:
+    return cave_name.lower() == cave_name
+
+def task1(connections: dict) -> None:
+    visited_caves = set()
+    visited_caves.add("start")
+
+    count = 0
+
+    def dfs(current_node: str = "start") -> None:
+
+        nonlocal count
+
+        if current_node == "end":
+            count += 1
+            return
+
+        for next_node in connections[current_node]:
+
+            if next_node not in visited_caves:
+                
+                if is_small(next_node):
+                    visited_caves.add(next_node)
+                    dfs(next_node)
+                    visited_caves.remove(next_node)
+
+                else:
+                    dfs(next_node)
+
+    dfs()
+
+    print(count)
+
+def main() -> None:
+    
+    connections = dict()
+
+    with open("input.txt", "r") as f:
+        lines = [line.split("-") for line in f.read().split("\n") if line.strip()]
+
+        for _from, to in lines:
+            if connections.get(_from) is None:
+                connections[_from] = list()
+
+            connections[_from].append(to)
+
+            if connections.get(to) is None:
+                connections[to] = list()
+
+            connections[to].append(_from)
+
+    task1(connections)
+
+
+if __name__ == '__main__':
+    main()
